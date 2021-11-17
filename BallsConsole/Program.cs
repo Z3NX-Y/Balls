dfdd3709148082d8f93591f9badc44f6f3ca5d27
Jonathan <72880844+ZENXDotPY@users.noreply.github.com>
2021-11-17 16:45:17 +0100

    Balls is ready

--- BallsConsole/Program.cs
+++ BallsConsole/Program.cs
@@ -2,17 +2,19 @@
 using System.IO;
 using System.Net;
 using BallsConsole.Functions;
+using System.Globalization;
 
 namespace BallsConsole
 {
     class Program
     {
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
             string desktop = @"C:\Users\" + Environment.UserName + @"\Desktop\";
             string localFilename = @"C:\Users\" + Environment.UserName + @"\Desktop\balls.jpg";
+            string logPath = @"C:\Users\" + Environment.UserName + @"\Documents\Balls\ballsLog.txt";
 
             try
             {
@@ -20,7 +22,7 @@ namespace BallsConsole
                 {
                     client.DownloadFile("https://i.redd.it/41ps5f0pnt771.png", localFilename);
                 }
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < 10000; i++)
                 {
                     string num = desktop + i.ToString() + ".jpg";
                     File.Copy(localFilename, num);
@@ -28,12 +30,13 @@ namespace BallsConsole
                     Console.WriteLine("Ball " + num);
                 }
 
+                
 
             }
 
             catch
             {
-
+                
             }
 
             Wallpaper.changeWallpaper();
